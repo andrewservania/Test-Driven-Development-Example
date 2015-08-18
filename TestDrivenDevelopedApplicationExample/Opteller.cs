c9c8071ed82411449afc11b26ae6f9c6cf436be5
Andrew Servania <ajp.servania@outlook.com>
2015-08-19 00:46:00 +0200

    + Initial commit (Dutch): Een simpele console applicatie met een unit test project om Test Driven Development aan te tonen in combinatie met Visual Studio z'n Unit Testing Framework.

---
+++ TestDrivenDevelopedApplicationExample/Opteller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopedApplicationExample
+{
+    /// <summary>
+    /// Simpele class die verantwoordelijk is voor optellen van de getallen
+    /// </summary>
+    public class Opteller
+    {
+        /// <summary>
+        /// Methode om getallen op te tellen
+        /// </summary>
+        /// <param name="getal1">Het eerste getal om op te tellen</param>
+        /// <param name="getal2">Het tweede getal om op te tellen</param>
+        /// <returns>Uitkomst van de opgetelde getallen</returns>
+        public int getallenOptellen(int getal1, int getal2)
+        {
+            return getal1 + getal2;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopedApplicationExample;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        /// <summary>
+        /// Test om te kijke of getallen goed opgeteld worden.
+        /// Test-Driven Development (TDD):
+        /// http://agiledata.org/essays/tdd.html
+        /// </summary>
+        [TestMethod]
+        public void GetalKanOpgeteldWorden()
+        {
+            //Geef aan het verwachte resultaat
+            int expectedResult = 7;
+
+            //Maak een instantie aan van de opteller class
+            Opteller opteller = new Opteller();
+
+            //Voor een simpele berekening uit om de gemaakt methode te testen.
+            int actualResult = opteller.getallenOptellen(2, 5);
+
+            //Laat de unit test project samen met de Unit Test Framework zelf checken
+            //of de uiteindelijke resultaat hetzelfde is als de verwachte resultaat.
+            //Als dat niet het geval is, geef aan in de Test Explorer aan met een
+            //simpele bericht.
+            Assert.AreEqual(expectedResult, actualResult,
+                "Getallen worden niet goed opgeteld. De antwoord is niet het verwachte resultaat");
+
+
+        }
+    }
+}
